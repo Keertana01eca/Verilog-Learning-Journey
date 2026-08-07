ef1fe03ebaebba56a7f0f2b928c9e511153ca6f0
Keertanaadapa <keertanaadapa@gmail.com>
2026-08-07 20:03:37 +0530

    Add files via upload

---
+++ Day-07-Half-Adder/tb.v
@@ -0,0 +1,15 @@
+//---------------------HalfAdder----------------------
+/*module tb;
+reg a,b;
+wire sum,carry;
+half_adder dut(a,b,sum,carry);
+initial
+begin
+	a=0;b=0;#10;
+	a=0;b=1;#10;
+	a=1;b=0;#10;
+	a=1;b=1;#10;
+end
+endmodule */
+
+
