b4e0d9d42ade93ac07eac554048cbc38d25e94e1
Keertanaadapa <keertanaadapa@gmail.com>
2026-09-10 19:07:33 +0530

    Add files via upload

---
+++ Day-21-1x4-Demux/rtl.v
@@ -0,0 +1,14 @@
+//1*4 Demux
+module demux4(input i,
+	input [1:0]s,
+	output reg [3:0]y);
+always@(*) begin
+	case(s)
+		2'b00:y=4'b0001;
+		2'b01:y=4'b0010;
+		2'b10:y=4'b0100;
+		2'b11:y=4'b1000;
+	endcase
+end
+endmodule
+
---
+++ Day-21-1x4-Demux/tb.v
@@ -0,0 +1,17 @@
+//1*4 Demux
+module tb;
+reg i;
+reg [1:0]s;
+wire [3:0]y;
+integer m;
+demux4 dut(i,s,y);
+initial
+begin
+	for(m=0;m<8;m=m+1)
+	begin
+		{s,i}=m;
+		#10;
+	end
+end
+endmodule
+
