5a95ad2ad9e98f72fe849b1a3b40f6d76cd7a7af
Keertanaadapa <keertanaadapa@gmail.com>
2026-09-18 13:31:37 +0530

    Add files via upload

---
+++ Day-27-8x3_Encoder/tb.v
@@ -0,0 +1,23 @@
+//8*3 encoder
+module tb;
+reg [7:0]i;
+wire [2:0]y;
+encoder dut(i,y);
+initial
+begin
+i=8'b00000001;#10;
+i=8'b00000010;#10;
+i=8'b00000100;#10;
+i=8'b00001000;#10;
+i=8'b00010000;#10;
+i=8'b00100000;#10;
+i=8'b01000000;#10;
+i=8'b10000000;#10;
+end
+initial
+begin
+#300;
+$finish;
+end
+endmodule
+
